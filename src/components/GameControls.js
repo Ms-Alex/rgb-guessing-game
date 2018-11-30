07c17d15f52a05456c084dff6973bd60ccf93d58
Ms-Alex <ms.hernandeza1@gmail.com>
2018-11-29 21:40:02 -0500

    change difficulties to buttons

--- src/components/GameControls.js
+++ src/components/GameControls.js
@@ -3,19 +3,19 @@ import React, { Component } from 'react';
 class GameControls extends Component {
     render() {
         return <div className="game-controls">
-            <span className="new-game" onClick={() => this.props.randomColors(6)}>
+            <button className="new-game" onClick={() => this.props.randomColors(6)}>
               {this.props.newGameText}
-            </span>
+            </button>
             <span className="progress-text">
               {this.props.progressText}
             </span>
-            <span className="level" onClick={() => this.props.gameDifficulty(3)}>
+            <button className="level" onClick={() => this.props.gameDifficulty(3)}>
               Easy
-            </span>
+            </button>
             &nbsp;
-            <span className="level" onClick={() => this.props.gameDifficulty(6)}>
+            <button className="level selected" onClick={() => this.props.gameDifficulty(6)}>
               Hard
-            </span>
+            </button>
           </div>;
     }
 }
