c438c97be679efeec93c267e34eb72e06c7f80d0
Ms-Alex <ms.hernandeza1@gmail.com>
2018-11-30 22:56:07 -0500

    Updates

---
+++ precache-manifest.a29f79992108e7d4556665e275feaa31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3f0679626cc08fc664",
+    "url": "/rgb-guessing-game/static/js/runtime~main.3a3f0679.js"
+  },
+  {
+    "revision": "9acdf7d17cbdafa95fd7",
+    "url": "/rgb-guessing-game/static/js/main.9acdf7d1.chunk.js"
+  },
+  {
+    "revision": "1964fcbc1f8e63b8251b",
+    "url": "/rgb-guessing-game/static/js/1.1964fcbc.chunk.js"
+  },
+  {
+    "revision": "9acdf7d17cbdafa95fd7",
+    "url": "/rgb-guessing-game/static/css/main.ef033824.chunk.css"
+  },
+  {
+    "revision": "983949e62b63935316fd9ea2e888b486",
+    "url": "/rgb-guessing-game/index.html"
+  }
+];
